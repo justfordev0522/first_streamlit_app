674a876964b4ca2fca1a535cfb574ed5d2e53eb5
justfordev0522 <104750633+justfordev0522@users.noreply.github.com>
2022-05-02 04:49:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit
 import requests
 
 fruityice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
